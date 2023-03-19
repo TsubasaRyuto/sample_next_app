2bf6f7c63544157a314d7ab8ea442ddd0bb543fb
TsubasaRyuto <tsubasa7ryuto@gmail.com>
2023-03-19 21:38:56 +0900

    api serverからデータfetchしてみた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,40 @@
-export default function Home() {
+const Home = ({ users }) => {
   return (
     <div>
-      <h1>Hello Smaple App!!</h1>
+      <h1>UserList</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.ID}>{user.Name}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
+};
+
+export async function getServerSideProps() {
+  try {
+    const response = await fetch('http://app:8080/api/users');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    const users = await data.list;
+
+    return {
+      props: {
+        users,
+      },
+    };
+  } catch (err) {
+    console.error('Fetch error:', err);
+
+    return {
+      props: {
+        users: [],
+      },
+    };
+  }
 }
+
+export default Home;
+
